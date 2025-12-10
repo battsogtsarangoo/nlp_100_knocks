ec732a44e4de2a84eea51dcdc6dc0c238138111d
battsogtsarangoo <k520521y@mail.cc.ehime-u.ac.jp>
2025-12-10 11:49:51 +0900

    remove 6 chapter6

--- chapter6/52.py
+++
@@ -1,9 +0,0 @@
-from gensim.models import KeyedVectors
-
-model_path = "GoogleNews-vectors-negative300.bin"
-model = KeyedVectors.load_word2vec_format(model_path, binary=True)
-
-results = model.most_similar("United_States", topn=10)
-
-for word, score in results:
-    print(word, score)
--- chapter6/53.py
+++
@@ -1,9 +0,0 @@
-from gensim.models import KeyedVectors
-
-path_model = "GoogleNews-vectors-negative300.bin"
-model = KeyedVectors.load_word2vec_format(path_model, binary = True)
-
-result = model.most_similar(positive = ['Spain', 'Athens'], negative = ['Madrid'], topn = 10)
-
-for word, score in result:
-    print(word, score)
